efb34d0916c6859d99fb29e6435885c2be45a158
phuochoan <89370644+PhuocHoan@users.noreply.github.com>
2024-11-18 15:39:54 +0700

    update minor version in dependency

--- app.js
+++ app.js
@@ -9,17 +9,17 @@ app.set("view engine", "ejs");
 const PORT = process.env.PORT ?? 1111;
 
 const server = app.listen(PORT, () => {
-    console.log(`TechKit starts at port ${PORT}`);
+  console.log(`TechKit starts at port http://localhost:${PORT}`);
 });
 
 process.on("SIGINT", () => {
-    server.close(() => console.log("Exit Server Express"));
+  server.close(() => console.log("Exit Server Express"));
 });
 
 // Init routes
 var router = require("./src/routes/index.js");
 app.set("views", path.join(__dirname, "src/views"));
-app.use("", router);
+app.use("/", router);
 
 // Use static files
 app.use(express.static("assets"));
