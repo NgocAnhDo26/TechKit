c8338f783cc5fcc07711bdcb68961696d76745e1
NgocAnhDo26 <dotanngocanh@gmail.com>
2025-01-05 17:59:54 +0700

    fix: product filters not working correctly

--- public/javascripts/productFilter.js
+++ public/javascripts/productFilter.js
@@ -87,7 +87,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 });
 
-/* Shop page filter*/
+/* Shop page filter */
 document.addEventListener('DOMContentLoaded', function () {
   const selectElement = document.querySelector('select');
   const arrowDown = document.getElementById('arrow-down');
@@ -153,54 +153,76 @@ const onFilterSubmit = async function (e) {
     : queryParams.delete('maxPrice');
 
   queryParams.delete('page');
+
   // Redirect
   const baseUrl = window.location.href.split('?')[0];
   url = `${baseUrl}${queryParams ? '?' : ''}${queryParams.toString()}`;
-  window.history.replaceState(null, '', url);
+  window.location.href = url;
 
-  const api_url = `${window.location.protocol}//${window.location.host}/api/v1/products${queryParams ? '?' : ''}${queryParams.toString()}`;
-  try {
-    const response = await fetch(api_url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    const { products, totalPage, currentPage } = await response.json();
-    console.log(products);
-    // Render products
-    const productList = document.getElementById('product-list');
-    productList.innerHTML = ''; // Clear existing products
-    products.forEach((product) => {
-      const productElement = document.createElement('div');
-      productElement.classList.add('bg-white', 'p-4', 'rounded-lg', 'shadow');
-      productElement.innerHTML = `
-                <img
-                    src="${product.product_image[0]?.url || ''}"
-                    alt="${product.name}"
-                    class="w-full object-cover mb-4 rounded-lg"
-                />
-                <a href="/shop/${product.category.name.toLowerCase()}/${product.id}" class="text-lg font-semibold mb-2">
-                    ${product.name}
-                </a>
-                <p class="my-2">${product.category.name}</p>
-                <div class="flex items-center mb-4">
-                    ${
-                      product.price_sale
-                        ? `
-                        <span class="text-lg font-bold text-primary">${product.price_sale.toLocaleString()}₫</span>
-                        <span class="text-sm font-bold line-through ml-2">${product.price.toLocaleString()}₫</span>
-                        <span class="text-sm font-bold text-primary ml-2">-${100 - Math.round((product.price_sale / product.price) * 100)}%</span>
-                    `
-                        : `<span class="text-lg font-bold text-primary">${product.price.toLocaleString()}₫</span>`
-                    }
-                </div>
-                <button class="bg-primary border border-transparent hover:bg-transparent hover:border-primary text-white hover:text-primary font-semibold py-2 px-4 rounded-full w-full">
-                    Thêm vào giỏ
-                </button>
-            `;
-      updatePaginationUI(currentPage, totalPage);
-      productList.appendChild(productElement);
-    });
-  } catch (error) {
-    console.error('Error fetching or rendering products:', error.message);
-  }
+  // const api_url = `${window.location.protocol}//${window.location.host}/api/product${queryParams ? '?' : ''}${queryParams.toString()}`;
+  // try {
+  //   const response = await fetch(api_url);
+  //   if (!response.ok) {
+  //     throw new Error(`Response status: ${response.status}`);
+  //   }
+  //   const { products, totalPage, currentPage } = await response.json();
+
+  //   // Render products
+  //   const productList = document.getElementById('product-list');
+  //   productList.innerHTML = ''; // Clear existing products
+  //   products.forEach((product) => {
+  //     const productElement = document.createElement('div');
+  //     productElement.classList.add('bg-white', 'p-4', 'rounded-lg', 'shadow', 'flex', 'flex-col', 'flex-1');
+  //     productElement.innerHTML += `
+  //       <img
+  //         src="${product.profile_img.url}"
+  //         alt="${product.name}"
+  //         class="w-full object-cover mb-4 rounded-lg"
+  //       />
+  //       <a
+  //         href="/shop/laptop/${product.id}"
+  //         class="text-lg font-semibold"
+  //       >
+  //         ${product.name}
+  //       </a>
+  //       <p class="my-2">${product.category}</p>
+  //       `;
+      
+  //     if (product.price_sale) {
+  //       productElement.innerHTML += `
+  //       <div class="flex items-center mb-4 mt-auto">
+  //         <span class="text-lg font-bold text-primary"
+  //           >${product.price_sale.toLocaleString()}₫</span
+  //         >
+  //         <span class="text-sm font-bold line-through ml-2"
+  //           >${product.price.toLocaleString()}₫</span
+  //         >
+  //         <span class="text-sm font-bold text-primary ml-2"
+  //           >-${Math.round((1 - product.price_sale / product.price) * 100)}%</span
+  //         >
+  //       </div>
+  //       `;
+  //     } else {
+  //       productElement.innerHTML += `
+  //       <div class="flex items-center mb-4 mt-auto">
+  //         <span class="text-lg font-bold text-primary"
+  //       >${product.price.toLocaleString()}₫}</span>
+  //       </div>
+  //       `;
+  //     }
+  //     productElement.innerHTML += `
+  //       <button
+  //         onclick="addToCartSingle(${product.id})"
+  //         class="bg-primary border border-transparent hover:bg-transparent hover:border-primary text-white hover:text-primary font-semibold py-2 px-4 rounded-full w-full"
+  //       >
+  //         Thêm vào giỏ
+  //       </button>
+  //     `;
+
+  //     updatePaginationUI(currentPage, totalPage);
+  //     productList.appendChild(productElement);
+  //   });
+  // } catch (error) {
+  //   console.error('Error fetching or rendering products:', error.message);
+  // }
 };
